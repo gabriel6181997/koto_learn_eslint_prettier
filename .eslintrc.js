b0395df982ba532b244ef8ad2fc7737fc16e0cfc
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-03 13:12:03 +0800

    ✨ Change eslint setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: "plugin:react/recommended",
+  extends: ["plugin:react/recommended"],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins:[],
   rules: {
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"],
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "no-undef": "error",
     "react/prop-types": "off",
+    "react/jsx-props-no-spreading": "off",
   },
 };
